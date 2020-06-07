0e0d8628ec80a80b7a525abfe0dd53b86446c98a
Ankit Sharma <10382058+anklinuxboy@users.noreply.github.com>
2020-06-07 07:23:43 -0400

    Added firebase cloud firestore message fetching and auth screen ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'screens/chat_screen.dart';
+import 'screens/auth_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,9 +13,18 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'ChatApp',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: ChatScreen(),
+          primarySwatch: Colors.pink,
+          backgroundColor: Colors.pink,
+          accentColor: Colors.deepPurple,
+          accentColorBrightness: Brightness.dark,
+          buttonTheme: ButtonTheme.of(context).copyWith(
+            buttonColor: Colors.pink,
+            textTheme: ButtonTextTheme.primary,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+          )),
+      home: AuthScreen(),
     );
   }
 }
--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,16 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: ListView.builder(
-        itemBuilder: (ctx, index) => Container(
-          padding: EdgeInsets.all(8),
-          child: Text('This works'),
-        ),
-        itemCount: 10,
+      body: StreamBuilder(
+        stream: Firestore.instance
+            .collection('chats/ETMIQnEmrcwPqGQifXyp/messages')
+            .snapshots(),
+        builder: (ctx, streamSnapshot) {
+          if (streamSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final docs = streamSnapshot.data.documents;
+          return ListView.builder(
+            itemBuilder: (ctx, index) => Container(
+              padding: EdgeInsets.all(8),
+              child: Text(docs[index]['text']),
+            ),
+            itemCount: streamSnapshot.data.documents.length,
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Firestore.instance
+              .collection('chats/ETMIQnEmrcwPqGQifXyp/messages')
+              .add({'text': 'floating action button clicked!'});
+        },
       ),
     );
   }
